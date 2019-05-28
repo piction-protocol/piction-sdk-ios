44669e44300cac9a89ad602b32570bf2b44e0f1a
jhSeo <jh.seo@battleent.com>
2019-05-28 10:13:35 +0900

    Change file path

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PictionSDK",
-            dependencies: ["Moya-ModelMapper", "KeychainAccess"]),
+            dependencies: ["Moya-ModelMapper", "KeychainAccess"],
+            path: "Sources"),
         .testTarget(
             name: "PictionSDKTests",
             dependencies: ["PictionSDK"]),

c9a65722867fed06125bf6fff2f072d786423310
jhSeo <jh.seo@battleent.com>
2019-05-29 18:28:23 +0900

    Update Package file

--- Package.swift
+++ Package.swift
@@ -5,21 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "PictionSDK",
+    platforms: [.iOS(.v8)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "PictionSDK",
             targets: ["PictionSDK"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/sunshinejr/Moya-ModelMapper.git", .upToNextMajor(from: "9.0.0")),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", .upToNextMajor(from: "3.2.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PictionSDK",
             dependencies: ["Moya-ModelMapper", "KeychainAccess"],

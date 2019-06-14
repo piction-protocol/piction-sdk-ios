004ba365ba903f4ff182cddb3b37bdd5fefb34fb
jhSeo <jh.seo@battleent.com>
2019-06-14 16:08:10 +0900

    Demo: Add SeriesAPI

---
+++ Demo/Piction-SDK-iOS-Example/ViewController/Series/SeriesUpdatePriorityViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SeriesUpdatePriorityViewController.swift
+//  Piction-SDK-iOS-Example
+//
+//  Created by jhseo on 14/06/2019.
+//  Copyright © 2019 Piction Network. All rights reserved.
+//
+
+import UIKit
+import PictionSDK
+
+class SeriesUpdatePriorityViewController: UIViewController {
+    @IBOutlet weak var projectIdTextField: UITextField!
+    @IBOutlet weak var seriesIdListTextField: UITextField!
+
+    @IBOutlet weak var responseTextView: UITextView!
+    @IBOutlet weak var executeButton: UIButton!
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    var isLoading: Bool = false {
+        didSet(loading) {
+            loading ? self.activityIndicator.stopAnimating() : self.activityIndicator.startAnimating()
+            self.executeButton.isEnabled = loading
+        }
+    }
+
+    @IBAction func executeBtnPressed(_ sender: Any) {
+        self.responseTextView.text = ""
+        self.isLoading = true
+
+        let idListArray = seriesIdListTextField.text?.components(separatedBy: ",")
+        let seriesIdList = idListArray?.compactMap { Int($0)! } ?? []
+
+        PictionSDK.series.updatePriority(projectId: projectIdTextField.text ?? "", seriesIdList: seriesIdList,
+            success: { response in
+                self.responseTextView.text = String(describing: response)
+                self.isLoading = false
+        },
+            failure: { error in
+                self.responseTextView.text = String(describing: error)
+                self.isLoading = false
+        })
+    }
+}

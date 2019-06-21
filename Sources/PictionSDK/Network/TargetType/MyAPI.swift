a8c7062ad7174e9bc55cf33724a793e8637574e5
jhSeo <jh.seo@battleent.com>
2019-06-21 15:12:32 +0900

    SDK: Add sales and subscription in MyApi

--- Sources/PictionSDK/Network/TargetType/MyAPI.swift
+++ Sources/PictionSDK/Network/TargetType/MyAPI.swift
@@ -14,6 +14,8 @@ public enum MyAPI {
     case projects
     case transactions(page: Int, size: Int)
     case wallet
+    case sales(salesMonth: Int, salesYear: Int)
+    case subscription(page: Int, size: Int)
 }
 
 extension MyAPI: TargetType {
@@ -21,18 +23,24 @@ extension MyAPI: TargetType {
     public var path: String {
         switch self {
         case .projects:
-            return "/my-projects"
+            return "/my/projects"
         case .transactions:
-            return "/my-transactions"
+            return "/my/transactions"
         case .wallet:
-            return "/my-wallet"
+            return "/my/wallet"
+        case .sales:
+            return "/my/sales"
+        case .subscription:
+            return "/my/subscription"
         }
     }
     public var method: Moya.Method {
         switch self {
         case .projects,
              .transactions,
-             .wallet:
+             .wallet,
+             .sales,
+             .subscription:
             return .get
         }
     }
@@ -44,6 +52,10 @@ extension MyAPI: TargetType {
             return jsonSerializedUTF8(json: PageViewResponse<TransactionModel>.sampleData())
         case .wallet:
             return jsonSerializedUTF8(json: WalletViewResponse.sampleData())
+        case .sales:
+            return jsonSerializedUTF8(json: [SalesViewResponse.sampleData()])
+        case .subscription:
+            return jsonSerializedUTF8(json: PageViewResponse<SubscriptionModel>.sampleData())
         }
     }
     public var task: Task {
@@ -51,12 +63,19 @@ extension MyAPI: TargetType {
         case .projects,
              .wallet:
             return .requestPlain
-        case .transactions(let page, let size):
+        case .transactions(let page, let size),
+             .subscription(let page, let size):
             let param = [
                 "page": page,
                 "size": size
             ]
             return .requestParameters(parameters: param, encoding: URLEncoding.queryString)
+        case .sales(let salesMonth, let salesYear):
+            let param = [
+                "salesMonth": salesMonth,
+                "salesYear": salesYear
+            ]
+            return .requestParameters(parameters: param, encoding: JSONEncoding.default)
         }
     }
     public var headers: [String: String]? {

bee32058b879dfcb093fa573f758c2c7c3082d23
jhSeo <jh.seo@battleent.com>
2019-07-10 17:32:32 +0900

    Demo: Update latest API in SearchAPI

---
+++ Demo/Piction-SDK-iOS-Example/ViewController/Search/SearchProjectViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SearchProjectViewController.swift
+//  Piction-SDK-iOS-Example
+//
+//  Created by jhseo on 10/07/2019.
+//  Copyright © 2019 Piction Network. All rights reserved.
+//
+
+import UIKit
+import PictionSDK
+
+class SearchProjectViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var pageTextField: UITextField!
+    @IBOutlet weak var sizeTextField: UITextField!
+
+    @IBOutlet weak var responseTextView: UITextView!
+    @IBOutlet weak var executeButton: UIButton!
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    var isLoading: Bool = false {
+        didSet(loading) {
+            loading ? self.activityIndicator.stopAnimating() : self.activityIndicator.startAnimating()
+            self.executeButton.isEnabled = loading
+        }
+    }
+
+    @IBAction func executeBtnPressed(_ sender: Any) {
+        self.responseTextView.text = ""
+        self.isLoading = true
+
+        PictionSDK.search.project(name: nameTextField.text ?? "", page: Int(pageTextField.text ?? "0") ?? 0, size: Int(sizeTextField.text ?? "0") ?? 0,
+            success: { response in
+                self.responseTextView.text = JsonUtil.toString(dict: response.toDict())
+                self.isLoading = false
+            },
+            failure: { error in
+                self.responseTextView.text = String(describing: error)
+                self.isLoading = false
+            })
+    }
+}
+

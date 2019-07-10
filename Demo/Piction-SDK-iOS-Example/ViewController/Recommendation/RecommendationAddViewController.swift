7c6d9cd8fc27941b4c6cd62b85f58fecaaaa8441
jhSeo <jh.seo@battleent.com>
2019-07-10 17:30:33 +0900

    Demo: Update latest API in RecommendationAPI

---
+++ Demo/Piction-SDK-iOS-Example/ViewController/Recommendation/RecommendationAddViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RecommendationAddViewController.swift
+//  Piction-SDK-iOS-Example
+//
+//  Created by jhseo on 14/06/2019.
+//  Copyright © 2019 Piction Network. All rights reserved.
+//
+
+import UIKit
+import PictionSDK
+
+class RecommendationAddViewController: UIViewController {
+    @IBOutlet weak var uriTextField: UITextField!
+
+    @IBOutlet weak var responseTextView: UITextView!
+    @IBOutlet weak var executeButton: UIButton!
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    var isLoading: Bool = false {
+        didSet(loading) {
+            loading ? self.activityIndicator.stopAnimating() : self.activityIndicator.startAnimating()
+            self.executeButton.isEnabled = loading
+        }
+    }
+
+    @IBAction func executeBtnPressed(_ sender: Any) {
+        self.responseTextView.text = ""
+        self.isLoading = true
+
+        PictionSDK.recommendation.add(uri: uriTextField.text ?? "",
+            success: { response in
+                self.responseTextView.text = JsonUtil.toString(dict: response.toDict())
+                self.isLoading = false
+        },
+            failure: { error in
+                self.responseTextView.text = String(describing: error)
+                self.isLoading = false
+        })
+    }
+}
+
---
+++ Demo/Piction-SDK-iOS-Example/ViewController/Recommendation/RecommendationAllViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RecommendationAllViewController.swift
+//  Piction-SDK-iOS-Example
+//
+//  Created by jhseo on 14/06/2019.
+//  Copyright © 2019 Piction Network. All rights reserved.
+//
+
+import UIKit
+import PictionSDK
+
+class RecommendationAllViewController: UIViewController {
+    @IBOutlet weak var sizeTextField: UITextField!
+
+    @IBOutlet weak var responseTextView: UITextView!
+    @IBOutlet weak var executeButton: UIButton!
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    var isLoading: Bool = false {
+        didSet(loading) {
+            loading ? self.activityIndicator.stopAnimating() : self.activityIndicator.startAnimating()
+            self.executeButton.isEnabled = loading
+        }
+    }
+
+    @IBAction func executeBtnPressed(_ sender: Any) {
+        self.responseTextView.text = ""
+        self.isLoading = true
+
+        PictionSDK.recommendation.all(size: Int(sizeTextField.text ?? "0") ?? 0,
+            success: { response in
+                self.responseTextView.text = JsonUtil.toString(dict: response.map { $0.toDict() })
+                self.isLoading = false
+            },
+            failure: { error in
+                self.responseTextView.text = String(describing: error)
+                self.isLoading = false
+            })
+    }
+}

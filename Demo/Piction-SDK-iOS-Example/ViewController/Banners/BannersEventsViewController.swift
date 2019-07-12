cda36e7fd4d9595ea07d2eddd3619dae7da44ba9
jhSeo <jh.seo@battleent.com>
2019-07-12 15:00:48 +0900

    Demo: Add BannersAPI example

---
+++ Demo/Piction-SDK-iOS-Example/ViewController/Banners/BannersEventsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  BannersEventViewController.swift
+//  Piction-SDK-iOS-Example
+//
+//  Created by jhseo on 12/07/2019.
+//  Copyright © 2019 Piction Network. All rights reserved.
+//
+
+import UIKit
+import PictionSDK
+
+class BannersEventsViewController: UIViewController {
+
+    @IBOutlet weak var responseTextView: UITextView!
+    @IBOutlet weak var executeButton: UIButton!
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    var isLoading: Bool = false {
+        didSet(loading) {
+            loading ? self.activityIndicator.stopAnimating() : self.activityIndicator.startAnimating()
+            self.executeButton.isEnabled = loading
+        }
+    }
+
+    @IBAction func executeBtnPressed(_ sender: Any) {
+        self.responseTextView.text = ""
+        self.isLoading = true
+
+        PictionSDK.banners.events(
+            success: { response in
+                self.responseTextView.text = JsonUtil.toString(dict: response.map { $0.toDict() })
+                self.isLoading = false
+        },
+            failure: { error in
+                self.responseTextView.text = String(describing: error)
+                self.isLoading = false
+        })
+    }
+}
+

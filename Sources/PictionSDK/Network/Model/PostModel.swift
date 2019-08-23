4938de9d6b2c80b06034d7ef1d00c08d579daf3d
jhSeo <jh.seo@battleent.com>
2019-08-23 14:09:14 +0900

    SDK: Change PostModel for fanpass

--- Sources/PictionSDK/Network/Model/PostModel.swift
+++ Sources/PictionSDK/Network/Model/PostModel.swift
@@ -13,21 +13,25 @@ public typealias PostViewResponse = PostModel
 
 public struct PostModel: Response {
     public let cover: String?
+    public let publishedAt: Date?
     public let createdAt: Date?
+    public let fanPass: FanPassModel?
     public let id: Int?
     public let likeCount: Int?
-    public let requiredSubscription: Bool?
     public let series: SeriesModel?
     public let title: String?
+    public let status: String?
 
     public init(map: Mapper) throws {
         cover = map.optionalFrom("cover")
+        publishedAt = map.optionalFrom("publishedAt")
         createdAt = map.optionalFrom("createdAt")
+        fanPass = map.optionalFrom("fanPass")
         id = map.optionalFrom("id")
         likeCount = map.optionalFrom("likeCount")
-        requiredSubscription = map.optionalFrom("requiredSubscription")
         series = map.optionalFrom("series")
         title = map.optionalFrom("title")
+        status = map.optionalFrom("status")
     }
 
     public func toJSONString() throws -> String {
@@ -37,12 +41,14 @@ public struct PostModel: Response {
     public func toDict() -> [String: Any?] {
         return [
             "cover": cover,
+            "publishedAt": publishedAt?.toString(format: "YYYY-MM-dd'T'HH:mm:ssZ"),
             "createdAt": createdAt?.toString(format: "YYYY-MM-dd'T'HH:mm:ssZ"),
+            "fanPass": fanPass?.toDict(),
             "id": id,
             "likeCount": likeCount,
-            "requiredSubscription": requiredSubscription,
             "series": series?.toDict(),
             "title": title,
+            "status": status
         ]
     }
 }
@@ -51,12 +57,14 @@ extension PostModel {
     static func sampleData() -> [String: Any] {
         return [
             "cover": "string",
+            "publishedAt": "2019-06-20T09:27:53.127+0000",
             "createdAt": "2019-06-20T09:27:53.127+0000",
+            "fanPass": FanPassModel.sampleData(),
             "id": 0,
             "likeCount": 0,
-            "requiredSubscription": true,
             "series": SeriesModel.sampleData(),
-            "title": "title"
+            "title": "title",
+            "status": "PUBLIC"
         ]
     }
 }

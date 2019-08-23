df6e8e9eea26f1767d12d8f6f47194a71a188f76
jhSeo <jh.seo@battleent.com>
2019-08-23 16:27:23 +0900

    Piction-iOS: Add MyProject in MyPage home

---
+++ Sources/PictionView/View/MyPage/MyProject/MyProjectViewController.swift
@@ -0,0 +1,105 @@
+//
+//  MyProjectViewController.swift
+//  PictionView
+//
+//  Created by jhseo on 12/07/2019.
+//  Copyright © 2019 Piction Network. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import ViewModelBindable
+import RxDataSources
+import PictionSDK
+
+final class MyProjectViewController: UITableViewController {
+    var disposeBag = DisposeBag()
+
+//    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var createProjectButton: UIBarButtonItem!
+
+    private func openProjectViewController(uri: String) {
+        let vc = ProjectViewController.make(uri: uri)
+        if let topViewController = UIApplication.topViewController() {
+            topViewController.openViewController(vc, type: .push)
+        }
+    }
+
+    private func openCreateProjectViewController(uri: String) {
+        let vc = CreateProjectViewController.make(uri: uri)
+        if let topViewController = UIApplication.topViewController() {
+            topViewController.openViewController(vc, type: .push)
+        }
+    }
+
+    private func configureDataSource() -> RxTableViewSectionedReloadDataSource<SectionModel<String, ProjectModel>> {
+        return RxTableViewSectionedReloadDataSource<SectionModel<String, ProjectModel>>(
+            configureCell: { (_, tv, ip, model) in
+                let cell: MyProjectTableViewCell = tv.dequeueReusableCell(forIndexPath: ip)
+                cell.configure(with: model)
+                return cell
+        }, canEditRowAtIndexPath: { (_, _) in
+            return true
+        })
+    }
+}
+
+extension MyProjectViewController: ViewModelBindable {
+
+    typealias ViewModel = MyProjectViewModel
+
+    func bindViewModel(viewModel: ViewModel) {
+        let dataSource = configureDataSource()
+
+        tableView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
+
+        let input = MyProjectViewModel.Input(
+            viewWillAppear: rx.viewWillAppear.asDriver(),
+            createProjectBtnDidTap: createProjectButton.rx.tap.asDriver(),
+            selectedIndexPath:
+            tableView.rx.itemSelected.asDriver()
+        )
+
+        let output = viewModel.build(input: input)
+
+        output
+            .projectList
+            .drive { $0 }
+            .map { [SectionModel(model: "", items: $0)] }
+            .bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+
+        output
+            .openCreateProjectViewController
+            .drive(onNext: { [weak self] _ in
+                self?.openCreateProjectViewController(uri: "")
+            })
+            .disposed(by: disposeBag)
+
+        output
+            .openProjectViewController
+            .drive(onNext: { [weak self] indexPath in
+                let uri = dataSource[indexPath].uri ?? ""
+                self?.openProjectViewController(uri: uri)
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+extension MyProjectViewController {
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let editAction = UIContextualAction(style: .normal, title: "편집", handler: { [weak self] (action, view, completionHandler) in
+                print("success")
+                if let item = self?.viewModel?.projectList[indexPath.row] {
+                    self?.openCreateProjectViewController(uri: item.uri ?? "")
+                    completionHandler(true)
+                } else {
+                    completionHandler(false)
+                }
+            })
+        return UISwipeActionsConfiguration(actions: [editAction])
+    }
+}

941a7306f7e64f1a660f37bcee32bbeefc5cc26a
jhSeo <jh.seo@battleent.com>
2019-08-23 16:20:32 +0900

    Piction-iOS: Add Utility source

---
+++ Sources/PictionView/Utility/Updater.swift
@@ -0,0 +1,28 @@
+//
+//  Updater.swift
+//  PictionView
+//
+//  Created by jhseo on 27/06/2019.
+//  Copyright © 2018 Piction Network. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+protocol UpdaterProtocol {
+    var refreshContent: PublishSubject<Void> { get }
+    var refreshSession: PublishSubject<Void> { get }
+    var refreshAmount: PublishSubject<Void> { get }
+}
+
+final class Updater: UpdaterProtocol {
+    let selectedProjectMenu = PublishSubject<Int>()
+    let refreshContent = PublishSubject<Void>()
+    let refreshSession = PublishSubject<Void>()
+    let refreshAmount = PublishSubject<Void>()
+
+    private let disposeBag = DisposeBag()
+
+    init() {}
+}
+

e68d03b8eff4f5dd1bf210f31ce750028a28ae68
jhSeo <jh.seo@battleent.com>
2019-11-25 15:19:47 +0900

    Update SubscriptionUser model

--- Sources/PictionSDK/Network/Model/SubscriptionUserModel.swift
+++ Sources/PictionSDK/Network/Model/SubscriptionUserModel.swift
@@ -12,12 +12,30 @@ import Foundation
 public typealias SubscriptionUserViewResponse = SubscriptionUserModel
 
 public struct SubscriptionUserModel: Response {
-    public let subscriptionDate: Date?
-    public let user: UserModel?
+    public let orderNo: Int?
+    public let subscriber: UserModel?
+    public let creator: UserModel?
+    public let fanPass: FanPassModel?
+    public let startedAt: Date?
+    public let expireDate: Date?
+    public let paidPrice: Int?
+    public let cpRevenue: Int?
+    public let createdAt: Date?
+    public let transactionStatus: String?
+    public let status: Bool?
 
     public init(map: Mapper) throws {
-        subscriptionDate = map.optionalFrom("subscriptionDate")
-        user = map.optionalFrom("user")
+        orderNo = map.optionalFrom("orderNo")
+        subscriber = map.optionalFrom("subscriber")
+        creator = map.optionalFrom("creator")
+        fanPass = map.optionalFrom("fanPass")
+        startedAt = map.optionalFrom("startedAt")
+        expireDate = map.optionalFrom("expireDate")
+        paidPrice = map.optionalFrom("paidPrice")
+        cpRevenue = map.optionalFrom("cpRevenue")
+        createdAt = map.optionalFrom("createdAt")
+        transactionStatus = map.optionalFrom("transactionStatus")
+        status = map.optionalFrom("status")
     }
 
     public func toJSONString() throws -> String {
@@ -26,8 +44,17 @@ public struct SubscriptionUserModel: Response {
 
     public func toDict() -> [String: Any?] {
         return [
-            "subscriptionDate": subscriptionDate?.toString(format: "YYYY-MM-dd'T'HH:mm:ssZ"),
-            "user": user
+            "orderNo": orderNo,
+            "subscriber": subscriber,
+            "creator": creator,
+            "fanPass": fanPass,
+            "startedAt": startedAt?.toString(format: "YYYY-MM-dd'T'HH:mm:ssZ"),
+            "expireDate": expireDate?.toString(format: "YYYY-MM-dd'T'HH:mm:ssZ"),
+            "paidPrice": paidPrice,
+            "cpRevenue": cpRevenue,
+            "createdAt": createdAt?.toString(format: "YYYY-MM-dd'T'HH:mm:ssZ"),
+            "transactionStatus": transactionStatus,
+            "status": status
         ]
     }
 }
@@ -35,8 +62,17 @@ public struct SubscriptionUserModel: Response {
 extension SubscriptionUserModel {
     static func sampleData() -> [String: Any] {
         return [
-            "subscriptionDate": "2019-06-20T09:27:53.127+0000",
-            "user": UserModel.sampleData()
+            "orderNo": 0,
+            "subscriber": UserModel.sampleData(),
+            "creator": UserModel.sampleData(),
+            "fanPass": FanPassModel.sampleData(),
+            "startedAt": "2019-06-20T09:27:53.127+0000",
+            "expireDate": "2019-06-20T09:27:53.127+0000",
+            "paidPrice": 0,
+            "cpRevenue": 0,
+            "createdAt": "2019-06-20T09:27:53.127+0000",
+            "transactionStatus": "SUCCESS",
+            "status": true,
         ]
     }
 }

7749779ed557cb4d8b466d6c651d1f7267176726
jhSeo <jh.seo@battleent.com>
2020-01-16 18:08:25 +0900

    Add unit test

--- Tests/PictionSDKTests/PictionSDKTests.swift
+++ Tests/PictionSDKTests/PictionSDKTests.swift
@@ -1,15 +1,50 @@
 import XCTest
+import Moya
+import Moya_ModelMapper
 @testable import PictionSDK
 
 final class PictionSDKTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PictionSDK().text, "Hello, World!")
+    var provider = MoyaProvider<UserAPI>()
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testProjects() {
+        let expectation = XCTestExpectation(description: "Request Test")
+
+        PictionSDK.user.findOne(id: "piction", success: { response in
+            XCTAssertNotNil(response)
+            expectation.fulfill()
+        }, failure: { error in
+            XCTFail()
+            expectation.fulfill()
+        })
+        wait(for: [expectation], timeout: 2)
+    }
+
+    func testTarget() {
+        let expectation = XCTestExpectation(description: "Target Test")
+        var responseData: UserModel?
+
+        provider.request(.findOne(id: "piction")) { result in
+            if case let .success(response) = result {
+                responseData = try? response.map(to: UserModel.self)
+                XCTAssertEqual(response.statusCode, 200)
+                XCTAssertNotNil(responseData)
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 2)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testProjects", testProjects),
+        ("testTarget", testTarget)
     ]
 }

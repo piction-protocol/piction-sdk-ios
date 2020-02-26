d44799e6c34a1434c9ac79d80136daf8426967ef
jhSeo <jh.seo@battleent.com>
2020-02-26 21:07:02 +0900

    Fix SubscriptionFanPassModel to SponsoredPlanModel for changed APIs

--- Sources/PictionSDK/Network/Model/SponsoredPlanModel.swift
+++ Sources/PictionSDK/Network/Model/SponsoredPlanModel.swift
@@ -1,5 +1,5 @@
 //
-//  SubscriptionFanPassModel.swift
+//  SponsoredPlanModel.swift
 //  PictionSDK
 //
 //  Created by jhseo on 2020/01/15.
@@ -9,17 +9,17 @@
 import Foundation
 import Mapper
 
-public typealias SubscriptionFanPassViewResponse = SubscriptionFanPassModel
+public typealias SponsoredPlanViewResponse = SponsoredPlanModel
 
-public struct SubscriptionFanPassModel: Response {
+public struct SponsoredPlanModel: Response {
     public let expireDate: Date?
-    public let fanPass: FanPassModel?
+    public let membership: MembershipModel?
     public let startedAt: Date?
     public let status: Bool?
 
     public init(map: Mapper) throws {
         expireDate = map.optionalFrom("expireDate")
-        fanPass = map.optionalFrom("fanPass")
+        membership = map.optionalFrom("membership")
         startedAt = map.optionalFrom("startedAt")
         status = map.optionalFrom("status")
     }
@@ -31,18 +31,18 @@ public struct SubscriptionFanPassModel: Response {
     public func toDict() -> [String: Any?] {
         return [
             "expireDate": expireDate?.toString(format: "YYYY-MM-dd'T'HH:mm:ssZ"),
-            "fanPass": fanPass,
+            "membership": membership,
             "startedAt": startedAt?.toString(format: "YYYY-MM-dd'T'HH:mm:ssZ"),
             "status": status,
         ]
     }
 }
 
-extension SubscriptionFanPassModel {
+extension SponsoredPlanModel {
     static func sampleData() -> [String: Any] {
         return [
             "expireDate": "2019-06-20T09:27:53.127+0000",
-            "fanPass": FanPassModel.sampleData(),
+            "membership": MembershipModel.sampleData(),
             "startedAt": "2019-06-20T09:27:53.127+0000",
             "status": true,
         ]
